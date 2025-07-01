b830ce7f5810fda3bdef6dbd3002072712527690
Arjun Kundu <kunduarjun02@gmail.com>
2025-07-01 16:23:38 -0300

    Got a lot of removal logic working

--- youtube-redux.js
+++ youtube-redux.js
@@ -1,23 +1,61 @@
+// Separate the right and left panels of the YouTube video player into two separate variables
 const leftSideDiv = document.getElementById('below'); 
 const rightSideDiv = document.getElementById('secondary');
 
-const idMapLeft = new Map();
-const idMapRight = new Map([['#companion', 'advertisement'], ['#panels', 'advertisement'], ['#scroll-container', 'relevant suggestion headers']]);
+// Map the tag ids with what they practically represent in the YouTube player 
+const idMapLeft = new Map([
+    ['#comments', 'comments']
+]);
+const idMapRight = new Map([
+    ['#companion', 'advertisement'], 
+    ['#panels', 'advertisement'], 
+    ['#scroll-container', 'relevant suggestion headers'],
+    ['#contents', 'suggested content for more watching']
+]);
 
+// Easy list for directions to use
+let directions = ["right", "left"]
 
-function removeByIdLeft(id) {
-    const testDiv = leftSideDiv.querySelector(id);
-}
+// Functions that allow for easy removal of sections on the YouTube player, based on the direction and id passed in as an arguments  
+function removeById(direction, id) {
+    let testDiv;
+    let testMap;
 
-function removeByIdRight(id) {
-    const testDiv = rightSideDiv.querySelector(id);
+    if (direction === "left") { testDiv = leftSideDiv.querySelector(id); testMap = idMapLeft; }
+    else if (direction === "right") { testDiv = rightSideDiv.querySelector(id); testMap = idMapRight; }
+    else { console.log(`Please enter in either right or left as your direction argument!`); return; }
 
     if (testDiv) {
         testDiv.remove();
-        console.log(`Tag with id '${id}' (which is an ${idMapRight[id]}) has been removed.`);
+        console.log(`Tag with id '${id}' (${testMap.get(id)}) has been removed.`);
     } else {
-        console.log(`Tag with id '${id}' (which is an ${idMapRight[id]}) could not be found.`);
+        console.log(`Tag with id '${id}' (${testMap.get(id)}) could not be found.`);
     }
 }
 
-removeByIdRight('#panels')
+// Function that allows for the removal of every section on a specified direction of the YouTube player
+function removeSectionsByMap(direction) {
+    let selectedMap;
+
+    if (direction === "left") { selectedMap = idMapLeft; }
+    else if (direction === "right") { selectedMap = idMapRight; }
+    else { console.log(`Please enter in either right or left as your direction argument!`); return; }
+
+    for (const key of selectedMap.keys()) {
+        removeById(direction, key);
+    }
+}
+
+// Concrete function call to remove all the sections specified in both the left and right id maps 
+for (const direction of directions) {
+    removeSectionsByMap(direction)
+}
+
+/*
+// Remove the whole right sided panel if there is no queue in use
+const queueCheckDiv = rightSideDiv.querySelector('#container'); 
+if (!queueCheckDiv) {
+    console.log(`No queue found, removing entire right side panel…`)
+    rightSideDiv.remove(); 
+}
+*/
